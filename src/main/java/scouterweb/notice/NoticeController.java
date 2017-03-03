8834be43db0685b444bbda09bb56b96f2676af82
Gunhee Lee <gunlee01@gmail.com>
2017-03-03 21:02:31 +0900

    notice 2017-03-03

--- src/main/java/scouterweb/notice/NoticeController.java
+++ src/main/java/scouterweb/notice/NoticeController.java
@@ -2,7 +2,10 @@ package scouterweb.notice;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author Gun Lee (gunlee01@gmail.com) on 2017. 3. 2.
@@ -11,8 +14,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class NoticeController {
 
     @GetMapping("/latest-notice")
-    public String getLatestNotice(Model model) {
-        model.addAttribute("name", "gunlee");
+    public String getLatestNotice(Model model, HttpServletResponse res) {
+        model.addAttribute("testAttr", "test-value");
+        res.setHeader("X-Scouter-ETag", DigestUtils.md5DigestAsHex("-scouter-20170303-01".getBytes()));
         return "latestNotice";
     }
 }
