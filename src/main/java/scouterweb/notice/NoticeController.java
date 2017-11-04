e3731b640c92d25862478576a3a940d6b5d735af
Gunhee Lee <gunlee01@gmail.com>
2017-11-04 17:27:09 +0900

    v1.8.0

--- src/main/java/scouterweb/notice/NoticeController.java
+++ src/main/java/scouterweb/notice/NoticeController.java
@@ -23,8 +23,8 @@ public class NoticeController {
 //            String name = headerNames.nextElement();
 //            log.info("header : {} -> {}", name, req.getHeader(name));
 //        }
-        model.addAttribute("testAttr", "test-value6");
-        res.setHeader("X-Scouter-ETag", DigestUtils.md5DigestAsHex("-scouter-20170821-01".getBytes()));
+        model.addAttribute("testAttr", "test-value7");
+        res.setHeader("X-Scouter-ETag", DigestUtils.md5DigestAsHex("-scouter-20171104-01".getBytes()));
         return "latestNotice";
     }
 
