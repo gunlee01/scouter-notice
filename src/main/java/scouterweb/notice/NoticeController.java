cc058e1ba23a0e5576131aa781a5756bc9eb57e6
Gunhee Lee <gunlee01@gmail.com>
2020-05-15 08:43:53 +0900

    v2.8.1

--- src/main/java/scouterweb/notice/NoticeController.java
+++ src/main/java/scouterweb/notice/NoticeController.java
@@ -29,15 +29,15 @@ public class NoticeController {
 //            String name = headerNames.nextElement();
 //            log.info("header : {} -> {}", name, req.getHeader(name));
 //        }
-        model.addAttribute("testAttr", "test-value20");
-        res.setHeader("X-Scouter-ETag", DigestUtils.md5DigestAsHex("-scouter-20190723-02".getBytes()));
+        model.addAttribute("testAttr", "test-value21");
+        res.setHeader("X-Scouter-ETag", DigestUtils.md5DigestAsHex("-scouter-20200515-01".getBytes()));
         return "latestNotice";
     }
 
     @GetMapping("/latest-notice2")
     public String getLatestNotice2(Model model, HttpServletRequest req, HttpServletResponse res) {
-        model.addAttribute("testAttr", "test-value20");
-        res.setHeader("X-Scouter-ETag", DigestUtils.md5DigestAsHex("-scouter-20190723-02".getBytes()));
+        model.addAttribute("testAttr", "test-value21");
+        res.setHeader("X-Scouter-ETag", DigestUtils.md5DigestAsHex("-scouter-20200515-01".getBytes()));
         return "latestNotice2";
     }
 
